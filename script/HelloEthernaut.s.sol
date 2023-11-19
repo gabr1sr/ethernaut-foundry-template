914df5f715654f25f7a5da1292c674f205ea5389
Gabriel Rosa <gabr1sr@proton.me>
2023-11-19 05:16:01 -0300

    fix: challenge 00 script template

--- script/HelloEthernaut.s.sol
+++ script/HelloEthernaut.s.sol
@@ -5,7 +5,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {Instance} from "../src/levels/HelloEthernaut.sol";
 
 contract HelloEthernautScript is Script {
-    Instance public instance = Instance(payable(/* instance address here */));
+    Instance public instance /* = Instance(payable(address here)) */;
 
     function run() external {
 	vm.startBroadcast();
